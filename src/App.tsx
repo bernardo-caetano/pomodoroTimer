b8acc005d00ca288367b34e9f23fda0e5b0f74ac
Bernardo Caetano <bernardocaetanofa@gmail.com>
2023-01-24 09:05:03 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { ThemeProvider } from "styled-components"
+
+import { defaultTheme } from "./styles/themes/default"
+
+export function App() {
+
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary"/>
+    </ThemeProvider>
+
+  )
+}
+
+
