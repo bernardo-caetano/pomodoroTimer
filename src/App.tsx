70c0aa9dfddcfcebad89850578e07e941d2f3ac8
Bernardo Caetano <bernardocaetanofa@gmail.com>
2023-01-31 08:17:02 -0300

    routes, layout and eslint added

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
-import { ThemeProvider } from "styled-components"
-import { Button } from "./components/Button"
-import { GlobalStyle } from "./styles/global"
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
 
-import { defaultTheme } from "./styles/themes/default"
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
-
-
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary"/>
-
-      <GlobalStyle/>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
     </ThemeProvider>
-
   )
 }
-
-
